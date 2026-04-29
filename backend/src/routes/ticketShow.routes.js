24bbf1cd005059f6d3057ad4dfb767986c90d273
Chanithi Perera <chanithidehansa@gmail.com>
2026-04-29 22:02:28 +0530

    Update ticketShow.routes.js

--- backend/src/routes/ticketShow.routes.js
+++ backend/src/routes/ticketShow.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const ticketShowController = require('../controllers/ticketShow.controller');
+const groupBookingRequestRoutes = require('./groupBookingRequest.routes');
 const { protect } = require('../middleware/auth.middleware');
 const { requireDatabase } = require('../middleware/db.middleware');
 const validateRequest = require('../validations/validateRequest');
 const { createBookingRules, bookingIdParamRules, verifyEntryRules } = require('../validations/ticketShow.validation');
 
 const router = express.Router();
+
+router.use('/group-requests', groupBookingRequestRoutes);
 
 router.get('/', ticketShowController.getModuleInfo);
 router.get('/catalog', requireDatabase, ticketShowController.getCatalog);
